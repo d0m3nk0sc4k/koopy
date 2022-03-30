5aae93262ffe736179066fa092725653fe905627
Domen Koščak <domen.koscak@gmail.com>
2022-03-30 13:48:09 +0200

    User endpoints + AI file

--- api/api.py
+++ api/api.py
@@ -5,27 +5,10 @@ from resources.user import *
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(User, '/api/user/<string:user_id>')
-
-"""
-EXAMPLE
----------------------------------------------------
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
-
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
-
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-
-api.add_resource(TodoSimple, '/<string:todo_id>')
-"""
+api.add_resource(UserInfo, '/api/user/<string:user_id>')
+api.add_resource(UserRegistration, '/api/user/register')
+api.add_resource(UserLogin, '/api/user/login')
+api.add_resource(UserDelete, '/api/user/delete')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
--- api/resources/user.py
+++ api/resources/user.py
@@ -1,7 +1,56 @@
+from flask import request
 from flask_restful import Resource
-from ...database.tables import *
+from database.tables import *
+from datetime import date
 
-class User(Resource):
+class UserInfo(Resource):
     def get(__self__, user_id):
-        db.connect()
         return {'text': user_id}, 200
+
+class UserRegistration(Resource):
+    def post(__self__):
+        try:
+            data = request.get_json(force=True)
+            print(data)
+        except:
+            return {"message": "Body has to have JSON data with user credentials."}, 400
+
+        user = User.select().where(User.mail == data["mail"])
+
+        if user.exists():
+            db.close()
+            return {"message": "User with that mail already exists"}, 403
+        
+        User.create(name=data["name"], mail=data["mail"], password=data["password"], last_login=date.today())
+        
+        return {"status": "ok"}, 200
+
+class UserLogin(Resource):
+    def get(__self__):
+        try:
+            data = request.get_json(force=True)
+        except:
+            return {"message": "Body has to have JSON data with user credentials."}, 400
+
+        user = User.select().where(User.mail == data["mail"])
+
+        if not user.exists():
+            return {"message": "User with that mail does not exist"}, 403
+
+        password = User.get(User.mail == data["mail"])
+
+        if (data["password"] == password.password):
+            return {"message": "ok"}, 200
+        else:
+            return {"message": "Wrong password"}, 403
+
+class UserDelete(Resource):
+    def post(__self__):
+        try:
+            data = request.get_json(force=True)
+        except:
+            return {"message": "Body has to have JSON data with user credentials."}, 400
+
+        User.delete().where(User.mail == data["mail"]).execute()
+
+        return {"message": "User successfully deleted"}, 200
