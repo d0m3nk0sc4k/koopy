d0b3b69f766766164f7399fffdeba31c29faf01c
Domen Koščak <domen.koscak@gmail.com>
2022-04-25 10:38:42 +0200

    Redesign cleanup

--- app/koopy/lib/main.dart
+++ app/koopy/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import 'package:koopy/components/Home.dart';
-import 'package:koopy/components/login/login.dart';
 import 'package:koopy/components/theme.dart';
 
 void main() => runApp(
       GetMaterialApp(
-        home: const Login(),
+        home: Scaffold(
+          body: Center(
+            child: Text("ADS"),
+          ),
+        ),
         theme: theme,
       ),
     );
