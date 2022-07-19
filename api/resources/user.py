e2d9056250a46f58f8fdf12e0a890f65b8ebc717
Domen Koščak <domen.koscak@gmail.com>
2022-07-19 09:28:54 +0200

    d

--- api/resources/user.py
+++ api/resources/user.py
@@ -15,6 +15,7 @@ class UserFamilies(Resource):
     def get(__self__, user_id):
         user = User.select().where(User.id == user_id).get()
         families = user.families.execute()
+        print(families, list(families))
         return list(families)
 
         toReturn = dict()
