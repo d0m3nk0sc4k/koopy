a7fdea87fee5aacd4538ecd3d3b743e77af3a1af
Domen Koščak <domen.koscak@gmail.com>
2022-07-19 09:35:53 +0200

    dsa

--- api/resources/user.py
+++ api/resources/user.py
@@ -20,7 +20,8 @@ class UserFamilies(Resource):
         toReturn = dict()
 
         for family in families:
-            familyName = Family.select(Family.name).where(Family.id == family.id_f).get()
+            familyName = Family.get(Family.name).where(Family.id == family.id_f)
+            print(familyName)
             toReturn[familyName] = family
 
         return dumps(toReturn), 200, {'Access-Control-Allow-Origin': '*'}
