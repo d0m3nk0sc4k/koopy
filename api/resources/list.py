25f6195808d7ba9a9456c229119e254c2b1b6a47
Domen Koščak <domen.koscak@gmail.com>
2022-07-29 09:22:59 +0200

    asd

--- api/resources/list.py
+++ api/resources/list.py
@@ -14,7 +14,9 @@ class ListRemoveItem(Resource):
         data = check_for_data()
         print(data)
         q = List_has_Product.select().where(List_has_Product.id_l == data["list_id"], List_has_Product.id_p == data["product_id"]).get()
-        q.delete().execute()
+        for i in q:
+            print(i)
+        #q.delete().execute()
         return {"message": "Product successfully removed"}, 204
 
 class ListInfo(Resource):
