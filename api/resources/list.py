0f341c1e71d4e0ee1ab9b2c6a04298d2afd5e222
Domen Koščak <domen.koscak@gmail.com>
2022-07-29 09:36:16 +0200

    asd

--- api/resources/list.py
+++ api/resources/list.py
@@ -12,8 +12,8 @@ class ListRemoveItem(Resource):
     @jwt_required()
     def delete(__self__):
         data = check_for_data()
-        q = List_has_Product.select().where(List_has_Product.id_l == data["list_id"] and List_has_Product.id_p == data["product_id"])
-        q.delete().execute()
+        q = List_has_Product.delete().where(List_has_Product.id_l == data["list_id"] and List_has_Product.id_p == data["product_id"])
+        q.execute()
         return {"message": "Product successfully removed"}, 204
 
 class ListInfo(Resource):
