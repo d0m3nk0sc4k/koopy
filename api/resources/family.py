1d85afc616982096be083ac6835619e6481ae82f
Domen Koščak <domen.koscak@gmail.com>
2022-08-22 09:33:53 +0200

    asd

--- api/resources/family.py
+++ api/resources/family.py
@@ -107,6 +107,6 @@ class GetLists(Resource):
     def get(__self__, id):
         lists = Family.select().where(Family.id == id).get()
 
-        print(lists)
+        print(lists.lists)
 
         return lists.lists
